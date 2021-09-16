4ec30d31966fa7233fa1ace67f89f6e06d60256c
JaffarMohammed <72737575+JafferAlmoalem@users.noreply.github.com>
2021-09-16 22:35:58 +0300

    Update Kelvin Weather.js

--- JavaScript Syntax, Part I/Kelvin Weather.js
+++ JavaScript Syntax, Part I/Kelvin Weather.js
@@ -1,3 +1,16 @@
+/*
+Kelvin Weather
+Deep in his mountain-side meteorology lab, the mad scientist Kelvin has mastered weather prediction.
+
+Recently, Kelvin began publishing his weather forecasts on his website. However there’s a problem: All of his forecasts describe the temperature in Kelvin.
+
+With our knowledge of JavaScript, let’s convert Kelvin to Celsius, then to Fahrenheit.
+
+Kelvin, Celsius, and Fahrenheit thermometers
+For example, 283 K converts to 10 °C which converts to 50 °F.
+*/
+
+
 // The forecast today is 293 in Kelvin
 const kelvin = 293;
 //Convert from kelvin to celsius
